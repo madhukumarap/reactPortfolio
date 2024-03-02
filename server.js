455127c7fc8fc77c49b273cc3bdd96862096c5d4
madhukumarap <madhukumarap07@gmail.com>
2024-03-02 09:51:06 +0530

    updates

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express()
 const port = process.env.PORT  || 8080;
 app.listen(port,()=>{
-    console.log("server is runinng sucessfully")
+    console.log(`server is runinng sucessfully , ${port}`)
 })
